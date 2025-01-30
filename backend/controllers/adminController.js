20de1027ee0db56f676d2fffbd8dd11e23b02762
ankit <ankitkarapurkar444@gmail.com>
2025-01-30 09:20:24 -0800

    multer updated

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,4 +1,5 @@
 import Doctor from '../models/doctorModel.js'
+import jwt from 'jsonwebtoken';
 import { v2 as cloudinary } from 'cloudinary';
 
 
@@ -63,4 +64,31 @@ export const addDoctor = async (req, res) => {
     console.error(error);
     res.status(500).json({ success: false, message: "Error adding doctor", error: error.message });
   }
+};
+
+
+
+
+export const adminLogin = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ success: false, message: 'Email and password are required' });
+        }
+
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+
+            const token = jwt.sign({ email }, process.env.JWT_SECRET, { expiresIn: '1h' });
+
+            return res.status(200).json({
+                success: true,
+                message: 'Login successful',
+                token
+            });
+        } else {
+            return res.status(401).json({ success: false, message: 'Invalid credentials' });
+        }
+    } catch (error) {
+        return res.status(500).json({ success: false, message: 'Server error', error: error.message });
+    }
 };
--- backend/routes/adminRoutes.js
+++ backend/routes/adminRoutes.js
@@ -1,9 +1,11 @@
 import express from 'express';
-import { addDoctor } from '../controllers/adminController.js';
+import { addDoctor,adminLogin } from '../controllers/adminController.js';
 import upload from '../middleware/multer.js';
 
 const router = express.Router();  
 
 router.post('/add-doctor', upload.single('doctorImage'), addDoctor);
+router.post('/admin-login',adminLogin);
+
 
 export default router;
